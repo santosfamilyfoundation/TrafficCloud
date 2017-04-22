f35e6160116fff92a0f8c437eb08f16ebe02801d
youralien <ryan.louie@students.olin.edu>
2017-04-22 01:27:50 +0000

    Linting + removing unused functions

--- app/traffic_cloud_utils/plotting/visualization.py
+++ app/traffic_cloud_utils/plotting/visualization.py
@@ -5,7 +5,6 @@ import math
 import os
 
 import numpy as np
-from scipy import stats
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from thinkstats2 import Cdf
